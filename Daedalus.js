d60e4b2bb643e7005f163f7741ea2c4703672bb4
barrettand <barrett.and@husky.neu.edu>
2017-11-07 16:21:29 -0500

    Test Sun Emoji
    Added :sunny: integration

--- Daedalus.js
+++ Daedalus.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
  
 var app = express();
 var port = process.env.PORT || 1337;
+var warmth = 50;
  
 app.use(bodyParser.urlencoded({ extended: true }));
  
@@ -16,6 +17,24 @@ app.post('/hello', function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
     text : 'Hello ' + userName + ", welcome to the Daedalus ARG! I'm Daedalus, and I'll be your host!"
+  };
+
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+app.post('/:sunny:', function (req, res, next) {
+  var userName = req.body.user_name;
+  if(warmth < 100) {
+    warmth += 5;
+  }
+  else {
+    wamth = 100;
+  }
+  var botPayload = {
+    text : 'Yum! Thanks, ' + userName + "! My wamth is now at " + warmth + '%!'
   };
 
   if (userName !== 'slackbot') {
